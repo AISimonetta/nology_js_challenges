5209548bb16d9882f305d389c1c428e1930a478e
AISimonetta <info@simonettaalejandra.com>
2023-11-07 11:03:33 +0000

    first excercise done

--- js-challenge.js
+++ js-challenge.js
@@ -1,15 +1,23 @@
 // Write a function that takes a number and returns true if it is a positive number and false if it is a negative number.
-isNumberPositive(-1); // returns false
-isNumberPositive(10); // returns true
+
+const  isNumberPositive = (num) => {
+    if ( num >= 1) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(isNumberPositive(-1)); // returns false
+console.log(isNumberPositive(10)); // returns true
 // Write a function that takes a number of days and converts it into an age.
-convertDaysToAge(3650); // returns 10
-convertDaysToAge(6570); // returns 18
+// convertDaysToAge(3650); // returns 10
+// convertDaysToAge(6570); // returns 18
 // Write a function that takes three numbers and returns the largest of the three numbers.
-getLargestNumber(2 ,1, 4); // returns 4
-getLargestNumber(6,2,3); // returns 6
+// getLargestNumber(2 ,1, 4); // returns 4
+// getLargestNumber(6,2,3); // returns 6
 // Write a function that takes an array of names and returns the last name from the array of names.
-getLastName([”Charlie”, “Rob”, “Andy”]); // returns “Andy”
-getLastName(["Ash","Stu"]); // returns "Stu"
+// getLastName([”Charlie”, “Rob”, “Andy”]); // returns “Andy”
+// getLastName(["Ash","Stu"]); // returns "Stu"
 // Write a function that takes an array of numbers and returns true if all of the numbers are positive. It should return false if there are one or more negative numbers in the array.
-allNumbersPositive([2,4,5]); // returns true
-allNumbersPositive([-5,4,6]); // returns false
+// allNumbersPositive([2,4,5]); // returns true
+// allNumbersPositive([-5,4,6]); // returns false
