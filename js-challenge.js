b88d2f39363caa6894cc62adf6f4475ab2fa4f0e
AISimonetta <info@simonettaalejandra.com>
2023-11-07 13:54:41 +0000

    excercise 5 still not working

--- js-challenge.js
+++ js-challenge.js
@@ -45,13 +45,15 @@ getLastName(["Ash","Stu"]); // returns "Stu"
 // Write a function that takes an array of numbers and returns true if all of the numbers are positive. 
 //It should return false if there are one or more negative numbers in the array.
 
- let arrysOfNum = []
-const allNumbersPositive = () =>  {
-    if (arrysOfNum[index] >= 1) {
-        console.log(true)
-    } else {
-        console.log(false)
+let array = []
+const allNumbersPositive = (array) => {
+    for(var i = 0; i < array.length; i++){
+        if(array[i] < 0){
+            return false;
+        }
     }
+    return true;
 }
+
 allNumbersPositive([2,4,5]); // returns true
 allNumbersPositive([-5,4,6]); // returns false
