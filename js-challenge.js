505d223bc6a7e812cc5020280c2895aee28b5f03
AISimonetta <info@simonettaalejandra.com>
2023-11-07 11:10:23 +0000

    second excercise done

--- js-challenge.js
+++ js-challenge.js
@@ -1,17 +1,23 @@
 // Write a function that takes a number and returns true if it is a positive number and false if it is a negative number.
 
-const  isNumberPositive = (num) => {
-    if ( num >= 1) {
-        return true
-    } else {
-        return false
-    }
-}
-console.log(isNumberPositive(-1)); // returns false
-console.log(isNumberPositive(10)); // returns true
+// const  isNumberPositive = (num) => {
+//     if ( num >= 1) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// console.log(isNumberPositive(-1)); // returns false
+// console.log(isNumberPositive(10)); // returns true
+
 // Write a function that takes a number of days and converts it into an age.
-// convertDaysToAge(3650); // returns 10
-// convertDaysToAge(6570); // returns 18
+
+const convertDaysToAge = (numDays) => {
+    age = numDays / 365
+    console.log(age)
+}
+convertDaysToAge(3650); // returns 10
+convertDaysToAge(6570); // returns 18
 // Write a function that takes three numbers and returns the largest of the three numbers.
 // getLargestNumber(2 ,1, 4); // returns 4
 // getLargestNumber(6,2,3); // returns 6
